d55e796ba8347ee28b22138eaa5544423f6220d4
priyanshu-Vedanta <priyanshu@vedantacore.com>
2025-10-03 11:52:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { useState } from 'react';
+function App() {
+
+  const [isDark, setIsDark] = useState(false);
+
+  return (
+    <div className={isDark ? 'dark-theme' : ''} style={{ background: 'var(--bg-color)', color: 'var(--text-color)' }}>
+      <h1>Hello Themed World</h1>
+      <button onClick={() => setIsDark(!isDark)}>Toggle Theme</button>
+    </div>
+  );
+}
+ 
+export default App
